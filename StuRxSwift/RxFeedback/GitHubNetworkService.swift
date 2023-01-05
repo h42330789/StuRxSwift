5d1d1fb9e25510007d673db5abdd435c42c8de86
aaa <aaabbbccc@gmail.com>
2023-01-05 15:22:52 +0800

    update: format

--- StuRxSwift/RxFeedback/GitHubNetworkService.swift
+++ StuRxSwift/RxFeedback/GitHubNetworkService.swift
@@ -11,6 +11,33 @@ import RxCocoa
 import ObjectMapper
 import Moya
 
+typealias SearchRepositoriesResponse = GitHubResult<GitHubRepositories, GitHubServiceError>
+
+// 响应结果枚举
+enum GitHubResult<T, E: Error> {
+    case success(T) // 成功 （里面是返回的数据）
+    case failure(E) // 失败 （里面是错误原因）
+}
+
+// 失败情况枚举
+enum GitHubServiceError: Error {
+    case offline
+    case githubLimitReached
+}
+
+// 失败枚举对应的错误信息
+extension GitHubServiceError {
+    var displayMessage: String {
+        switch self {
+        case .offline:
+            return "网络连接失败！"
+        case .githubLimitReached:
+            return "请求太频繁，请稍后再试！"
+        }
+    }
+}
+
+
 class GitHubNetworkService {
     // 验证用户名是否存在
     func usernameAvailable(_ username: String) -> Observable<Bool> {
@@ -46,6 +73,7 @@ class GitHubNetworkService {
                 return Observable<GitHubRepositories>.empty()
             }
     }
+    
     static func searchResponstiresDriver(query: String) -> Driver<GitHubRepositories> {
         return GitHubProvider.rx.request(.repositories(query))
             .filterSuccessfulStatusCodes()
@@ -55,6 +83,7 @@ class GitHubNetworkService {
             }
             .asDriver(onErrorDriveWith: Driver.empty())
     }
+    
     func searchResponstiresDriver(query: String) -> Driver<GitHubRepositories> {
         return GitHubProvider.rx.request(.repositories(query))
             .filterSuccessfulStatusCodes()
@@ -64,5 +93,22 @@ class GitHubNetworkService {
             }
             .asDriver(onErrorDriveWith: Driver.empty())
     }
-        
+    
+    
+    // 搜索资源数据
+//    func searchRepositories2(query:String) -> Observable<SearchRepositoriesResponse> {
+//        return GitHubProvider.rx.request(.repositories(query))
+//            .filterSuccessfulStatusCodes()
+//            .mapJSON()
+//            .map {
+//                Mapper<GitHubRepositories>().map(JSON: ($0 as? [String: Any]) ?? [:], toObject: GitHubRepositories())
+//            }
+////            .map { .success($0) }
+////            .catch { error in
+////                print("发生错误：",error.localizedDescription)
+////                // 失败返回（GitHub接口对请求频率有限制，太频繁会被拒绝：403）
+////                return Observable.just(SearchRepositoriesResponse.failure(.githubLimitReached))
+////            }
+//    }
 }
+
--- StuRxSwift/RxFeedback/RxFeedBack1ViewController.swift
+++ StuRxSwift/RxFeedback/RxFeedBack1ViewController.swift
@@ -5,27 +5,27 @@
 //  Created by aaa on 12/30/22.
 //
 
-import UIKit
-import RxSwift
 import RxCocoa
 import RxFeedback
+import RxSwift
+import UIKit
 
 class RxFeedBack1ViewController: BaseViewController {
-    
     var addBtn: UIButton!
     var reduceBtn: UIButton!
     var label: UILabel!
     lazy var textView: UITextView = {
-        let v = UITextView(frame: CGRect(x: 100, y: 150, width: view.bounds.size.width-200, height: 150))
+        let v = UITextView(frame: CGRect(x: 100, y: 150, width: view.bounds.size.width - 200, height: 150))
         v.backgroundColor = .lightGray
         view.addSubview(v)
         return v
     }()
-    
+
     struct StateModel {
         var id: Int // 数字
         var content: String // 当前id对应的内容
     }
+
     typealias State = StateModel
     enum Event {
         case increment // 加一
@@ -46,13 +46,13 @@ class RxFeedBack1ViewController: BaseViewController {
             return label
         }()
         reduceBtn = createMyBtn(preView: label, title: "-")
-        
+
         // RxFeedback的核心方法
         Driver.system(
             // 初始状态
             initialState: StateModel(id: 0, content: ""),
             // 各个事件对状态的改变
-            reduce: { (state, event) in
+            reduce: { state, event in
                 switch event {
                 case .increment:
                     var result = state
@@ -69,38 +69,36 @@ class RxFeedBack1ViewController: BaseViewController {
                 }
             },
             // UI反馈
-            feedback: bind(self) { (me, state) -> Bindings<RxFeedBack1ViewController.Event> in
+            feedback: bind(self) { me, state -> Bindings<RxFeedBack1ViewController.Event> in
                 // 状态输出到页面控件上
                 let subscriptions = [
-                    state.map {"\($0.id)"}.drive(me.label.rx.text),
-                    state.map {"\($0.content)"}.drive(me.textView.rx.text)
+                    state.map { "\($0.id)" }.drive(me.label.rx.text),
+                    state.map { "\($0.content)" }.drive(me.textView.rx.text)
                 ]
                 // 将UI事件变成Event输入到反馈循环里面去
                 let events = [
-                    me.addBtn.rx.tap.map { Event.increment},
-                    me.reduceBtn.rx.tap.map { Event.decrement}
+                    me.addBtn.rx.tap.map { Event.increment },
+                    me.reduceBtn.rx.tap.map { Event.decrement }
                 ]
                 return Bindings(subscriptions: subscriptions, events: events)
             },
             // 非UI的自动反馈
-            react(request: {$0.id}, effects: {id in
-                return self.getContent(id: id)
-                    .asSignal(onErrorRecover: {_ in .empty()})
+            react(request: { $0.id }, effects: { id in
+                self.getContent(id: id)
+                    .asSignal(onErrorRecover: { _ in .empty() })
                     .map(Event.response)
             })
-        
         )
         .drive()
         .disposed(by: disposeBag)
-        
     }
-    
+
     func getContent(id: Int) -> Observable<String> {
         print("正在请求数据")
         return Observable.just("这是 id=\(id) 的新闻内容、。。")
             .delay(.seconds(1), scheduler: MainScheduler.instance)
     }
-    
+
     func createMyBtn(preView: UIView?, title: String) -> UIButton {
         let x = preView?.frame.maxX ?? 100
         let addBtn = UIButton(frame: CGRect(x: x, y: 100, width: 40, height: 40))
@@ -110,5 +108,4 @@ class RxFeedBack1ViewController: BaseViewController {
         view.addSubview(addBtn)
         return addBtn
     }
-    
 }
